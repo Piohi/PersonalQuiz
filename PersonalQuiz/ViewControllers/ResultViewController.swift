db16a54c3b057a01200d2aaf1f8b883fad9fceb5
Piohi <box1234567b@gmail.com>
2024-02-16 14:07:31 +0300

    sort array

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    var answers: [Answer]!
+    var animals: [Animal] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for answer in answers {
+            animals.append(answer.animal)
+        }
+        let count = animals.reduce(into: [:]) { $0[$1, default: 0] += 1 }
+        let result = count.sorted(by: { $0.1 > $1.1 }).first
 
+        
     }
     
     @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
